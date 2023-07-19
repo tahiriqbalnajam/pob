ccff35377fb3c24fe7120255749d791365047679
Tahir Iqbal Najam <tahiriqbal09@gmail.com>
2023-07-19 11:13:04 +0500

    init

---
+++ next.config.js
@@ -0,0 +1,19 @@
+
+if (!process.env.WORDPRESS_API_URL) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `)
+}
+
+module.exports = {
+  images: {
+    domains: [
+      new URL(process.env.WORDPRESS_API_URL).hostname,
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+    ],
+  },
+};
